6c4c4206d227dd7b3db0dc6093ab00f39dba5dcf
locht1 <locht1@runsystem.net>
2025-04-15 14:12:55 +0700

    Add config for jwt

--- src/main/java/vn/student_management/config/SwaggerConfig.java
+++ src/main/java/vn/student_management/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package vn.student_management.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +14,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("My Spring Boot API")
                         .version("1.0.0")
-                        .description("API documentation for my Spring Boot application"));
+                        .description("API with JWT authentication"));
     }
 }
